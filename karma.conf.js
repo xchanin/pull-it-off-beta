4132019ec670c8998dc1ac9da2466315185381cb
xchanin <shannonwbruns@gmail.com>
2021-12-20 15:17:11 -0700

    changing name to include beta

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pull-it-off-basic'),
+      dir: require('path').join(__dirname, './coverage/pull-it-off-beta'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pull-it-off-basic'`, () => {
+  it(`should have as title 'pull-it-off-beta'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pull-it-off-basic');
+    expect(app.title).toEqual('pull-it-off-beta');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pull-it-off-basic app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pull-it-off-beta app is running!');
   });
 });
